7b5c02c9e57e0053a8742c549524e075bb3bb61f
Chidi Nwanneka <cnwanneka@gmail.com>
2023-02-11 21:07:41 +0000

    added logic files

---
+++ assets/js/logic.js
@@ -0,0 +1,16 @@
+let imageContainer = document.querySelector(".img-container");
+
+// Listen for any clicks within the img-container div
+imageContainer.addEventListener("click", function(event) {
+    let element = event.target;
+
+    // 
+    if (condition) {
+        
+    }
+
+
+
+
+});
+
