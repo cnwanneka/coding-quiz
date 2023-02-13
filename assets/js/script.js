9e71641a6467ee30ae134406e561b731866b4689
Chidi Nwanneka <cnwanneka@gmail.com>
2023-02-13 14:55:03 +0000

    added script. js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,125 @@
+//Variables representing DOM elements
 
+let questionsEl = document.querySelector("#questions");
+let timerEl = document.querySelector("#timer");
+let choicesEl = document.querySelector("#options");
+let submitBtn = document.querySelector("#submit-score");
 let startBtn = document.querySelector("#start");
+let nameEl = document.querySelector("#name");
+let feedbackEl = document.querySelector("#feedback");
+let reStartBtn = document.querySelector("#restart");
 
 
+// Variables for the beginning part of the quiz.
+
+let currentQuestionIndex = 0;
+let time = questions.length * 15;
+let timerId;
+
+// Start of quiz
+
+function quizStart() {
+    timerId = setInterval(clockTick, 1000);
+    timerEl.textContent = time;
+    let landingScreenEl = document.getElementById("start-screen");
+    landingScreenEl.setAttribute("class", "hide");
+    questionsEl.removeAttribute("class");
+    getQuestion();
+}
+
+// Getting the current question from the array.
+
+function getQuestion() {
+    let currentQuestion = questions[currentQuestionIndex];
+  let promptEl = document.getElementById("question-words")
+    promptEl.textContent = currentQuestion.prompt;
+    choicesEl.innerHTML = "";
+    currentQuestion.options.forEach(function(choice, i) {
+        let choiceBtn = document.createElement("button");
+        choiceBtn.setAttribute("value", choice);
+        choiceBtn.textContent = i + 1 + ". " + choice;
+        choiceBtn.onclick = questionClick;
+        choicesEl.appendChild(choiceBtn);
+    });
+}
+
+// Checking if the answer is correct or not.
+
+function questionClick() {
+    if (this.value !== questions[currentQuestionIndex].answer) {
+      time -= 10;
+      if (time < 0) {
+        time = 0;
+      }
+      timerEl.textContent = time;
+      feedbackEl.textContent = `Wrong! The correct answer was ${questions[currentQuestionIndex].answer}.`;
+      feedbackEl.style.color = "red";
+    } else {
+      feedbackEl.textContent = "Correct!";
+      feedbackEl.style.color = "green";
+    }
+    feedbackEl.setAttribute("class", "feedback");
+    setTimeout(function() {
+      feedbackEl.setAttribute("class", "feedback hide");
+    }, 2000);
+    currentQuestionIndex++;
+    if (currentQuestionIndex === questions.length) {
+      quizEnd();
+    } else {
+      getQuestion();
+    }
+}
+
+// End of the quiz.
+
+function quizEnd() {
+    clearInterval(timerId);
+    let endScreenEl = document.getElementById("quiz-end");
+    endScreenEl.removeAttribute("class");
+    let finalScoreEl = document.getElementById("score-final");
+    finalScoreEl.textContent = time;
+    questionsEl.setAttribute("class", "hide");
+}
 
 
+// Quiz ends when timer reaches 0.
+
+function clockTick() {
+    time--;
+    timerEl.textContent = time;
+    if (time <= 0) {
+      quizEnd();
+    }
+}
+
+// Save the scores in the local storage.
+
+function saveHighscore() {
+    let name = nameEl.value.trim();
+    if (name !== "") {
+      let highscores =
+        JSON.parse(window.localStorage.getItem("highscores")) || [];
+      let newScore = {
+        score: time,
+        name: name
+      };
+      highscores.push(newScore);
+      window.localStorage.setItem("highscores", JSON.stringify(highscores));
+    }
+}
 
 
+function checkForEnter(event) {
+    if (event.key === "Enter") {
+        saveHighscore();
+    }
+}
+nameEl.onkeyup = checkForEnter;
 
+// Save user's score with submit button.
 
+submitBtn.onclick = saveHighscore;
 
-
-
-// Start the quiz when Start Quiz button is clicked
+// Restart the quiz.
 
 startBtn.onclick = quizStart;
